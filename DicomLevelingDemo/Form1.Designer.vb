a310a36d7c4f07d9ef2027ebb9aa34a4e7516d77
Atalasoft Support <developersupport@atalasoft.com>
2026-04-27 10:47:25 -0400

    initial commit

---
+++ DicomLevelingDemo/Form1.Designer.vb
@@ -0,0 +1,176 @@
+﻿Namespace DicomLevelingDemo
+    Partial Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+            If Not disposing AndAlso Not (components.Equals(Nothing)) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.imageViewer1 = New Atalasoft.Imaging.WinControls.ImageViewer
+            Me.menuStrip1 = New System.Windows.Forms.MenuStrip
+            Me.fileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+            Me.openToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+            Me.exitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+            Me.windowSlider = New System.Windows.Forms.TrackBar
+            Me.levelSlider = New System.Windows.Forms.TrackBar
+            Me.windowLabel = New System.Windows.Forms.Label
+            Me.levelLabel = New System.Windows.Forms.Label
+            Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+            Me.HelpAboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+            Me.menuStrip1.SuspendLayout()
+            CType(Me.windowSlider, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.levelSlider, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            '
+            'imageViewer1
+            '
+            Me.imageViewer1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                        Or System.Windows.Forms.AnchorStyles.Left) _
+                        Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+            Me.imageViewer1.AntialiasDisplay = Atalasoft.Imaging.WinControls.AntialiasDisplayMode.ScaleToGray
+            Me.imageViewer1.DisplayProfile = Nothing
+            Me.imageViewer1.Location = New System.Drawing.Point(12, 27)
+            Me.imageViewer1.Magnifier.BackColor = System.Drawing.Color.White
+            Me.imageViewer1.Magnifier.BorderColor = System.Drawing.Color.Black
+            Me.imageViewer1.Magnifier.Size = New System.Drawing.Size(100, 100)
+            Me.imageViewer1.Name = "imageViewer1"
+            Me.imageViewer1.OutputProfile = Nothing
+            Me.imageViewer1.Selection = Nothing
+            Me.imageViewer1.Size = New System.Drawing.Size(568, 444)
+            Me.imageViewer1.TabIndex = 0
+            Me.imageViewer1.Text = "imageViewer1"
+            '
+            'menuStrip1
+            '
+            Me.menuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.fileToolStripMenuItem, Me.HelpToolStripMenuItem})
+            Me.menuStrip1.Location = New System.Drawing.Point(0, 0)
+            Me.menuStrip1.Name = "menuStrip1"
+            Me.menuStrip1.Size = New System.Drawing.Size(592, 24)
+            Me.menuStrip1.TabIndex = 1
+            Me.menuStrip1.Text = "menuStrip1"
+            '
+            'fileToolStripMenuItem
+            '
+            Me.fileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.openToolStripMenuItem, Me.exitToolStripMenuItem})
+            Me.fileToolStripMenuItem.Name = "fileToolStripMenuItem"
+            Me.fileToolStripMenuItem.Size = New System.Drawing.Size(35, 20)
+            Me.fileToolStripMenuItem.Text = "File"
+            '
+            'openToolStripMenuItem
+            '
+            Me.openToolStripMenuItem.Name = "openToolStripMenuItem"
+            Me.openToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.O), System.Windows.Forms.Keys)
+            Me.openToolStripMenuItem.Size = New System.Drawing.Size(140, 22)
+            Me.openToolStripMenuItem.Text = "Open"
+            '
+            'exitToolStripMenuItem
+            '
+            Me.exitToolStripMenuItem.Name = "exitToolStripMenuItem"
+            Me.exitToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Q), System.Windows.Forms.Keys)
+            Me.exitToolStripMenuItem.Size = New System.Drawing.Size(140, 22)
+            Me.exitToolStripMenuItem.Text = "Exit"
+            '
+            'windowSlider
+            '
+            Me.windowSlider.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+            Me.windowSlider.LargeChange = 500
+            Me.windowSlider.Location = New System.Drawing.Point(13, 495)
+            Me.windowSlider.Maximum = 10000
+            Me.windowSlider.Name = "windowSlider"
+            Me.windowSlider.Size = New System.Drawing.Size(180, 42)
+            Me.windowSlider.TabIndex = 2
+            Me.windowSlider.TickFrequency = 500
+            '
+            'levelSlider
+            '
+            Me.levelSlider.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+            Me.levelSlider.LargeChange = 500
+            Me.levelSlider.Location = New System.Drawing.Point(400, 494)
+            Me.levelSlider.Maximum = 10000
+            Me.levelSlider.Name = "levelSlider"
+            Me.levelSlider.Size = New System.Drawing.Size(180, 42)
+            Me.levelSlider.TabIndex = 3
+            Me.levelSlider.TickFrequency = 500
+            '
+            'windowLabel
+            '
+            Me.windowLabel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+            Me.windowLabel.Location = New System.Drawing.Point(13, 475)
+            Me.windowLabel.Name = "windowLabel"
+            Me.windowLabel.Size = New System.Drawing.Size(180, 20)
+            Me.windowLabel.TabIndex = 4
+            Me.windowLabel.Text = "Window"
+            '
+            'levelLabel
+            '
+            Me.levelLabel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+            Me.levelLabel.Location = New System.Drawing.Point(400, 474)
+            Me.levelLabel.Name = "levelLabel"
+            Me.levelLabel.Size = New System.Drawing.Size(180, 20)
+            Me.levelLabel.TabIndex = 5
+            Me.levelLabel.Text = "Level"
+            '
+            'HelpToolStripMenuItem
+            '
+            Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HelpAboutToolStripMenuItem})
+            Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+            Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(40, 20)
+            Me.HelpToolStripMenuItem.Text = "Help"
+            '
+            'HelpAboutToolStripMenuItem
+            '
+            Me.HelpAboutToolStripMenuItem.Name = "HelpAboutToolStripMenuItem"
+            Me.HelpAboutToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+            Me.HelpAboutToolStripMenuItem.Text = "About ..."
+            '
+            'Form1
+            '
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(592, 531)
+            Me.Controls.Add(Me.levelSlider)
+            Me.Controls.Add(Me.windowSlider)
+            Me.Controls.Add(Me.windowLabel)
+            Me.Controls.Add(Me.levelLabel)
+            Me.Controls.Add(Me.imageViewer1)
+            Me.Controls.Add(Me.menuStrip1)
+            Me.MainMenuStrip = Me.menuStrip1
+            Me.Name = "Form1"
+            Me.Text = "Dicom Window and Leveling"
+            Me.menuStrip1.ResumeLayout(False)
+            Me.menuStrip1.PerformLayout()
+            CType(Me.windowSlider, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.levelSlider, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+            Me.PerformLayout()
+
+        End Sub
+
+        Private WithEvents imageViewer1 As Atalasoft.Imaging.WinControls.ImageViewer
+        Private WithEvents menuStrip1 As System.Windows.Forms.MenuStrip
+        Private WithEvents fileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+        Private WithEvents openToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+        Private WithEvents exitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+        Private WithEvents windowSlider As System.Windows.Forms.TrackBar
+        Private WithEvents levelSlider As System.Windows.Forms.TrackBar
+        Private WithEvents windowLabel As System.Windows.Forms.Label
+        Private WithEvents levelLabel As System.Windows.Forms.Label
+        Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+        Friend WithEvents HelpAboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    End Class
+End Namespace
---
+++ DicomLevelingDemo/Form1.vb
@@ -0,0 +1,160 @@
+﻿Imports System
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports Atalasoft.Imaging
+Imports Atalasoft.Imaging.Codec
+Imports Atalasoft.Imaging.Codec.Dicom
+Imports Atalasoft.Imaging.WinControls
+Imports System.IO
+
+Namespace DicomLevelingDemo
+    Partial Public Class Form1
+        Inherits Form
+        Private _stm As Stream
+        Private _dImage As DicomImage
+        Private _dataSet As DicomDataset
+        Private _image As AtalaImage
+
+        Public Sub New()
+            InitializeComponent()
+
+            ' Add the DicomDecoder to the registered decoders
+            RegisteredDecoders.Decoders.Add(New DicomDecoder())
+
+            ' hook into form closing so we can clean up
+            AddHandler Me.FormClosing, AddressOf Form1_FormClosing
+        End Sub
+
+        Sub Form1_FormClosing(ByVal sender As Object, ByVal e As FormClosingEventArgs)
+            ' close all open items
+            CloseCurrentItems()
+        End Sub
+
+        Private Sub openToolStripMenuItem_Click(ByVal sender As Object, ByVal e As EventArgs) Handles openToolStripMenuItem.Click
+            Dim oifd As New OpenImageFileDialog()
+            oifd.Title = "Select a Dicom Image"
+
+            If oifd.ShowDialog() = DialogResult.OK Then
+                Dim stm As FileStream = Nothing
+                Try
+                    ' open the file read-only
+                    stm = New FileStream(oifd.FileName, FileMode.Open, FileAccess.Read, FileShare.Read)
+
+                    ' Ensure that the file is a Dicom file
+                    Dim decoder As ImageDecoder = RegisteredDecoders.GetDecoder(stm)
+                    If decoder IsNot Nothing AndAlso TypeOf decoder Is DicomDecoder Then
+                        ' close anything open
+                        CloseCurrentItems()
+
+                        ' open new file
+                        OpenItems(stm)
+                    Else
+                        stm.Close()
+                        MessageBox.Show("Please choose a Dicom image instead.")
+                    End If
+                Catch
+                    If stm Is Nothing Then
+                        stm.Close()
+                    End If
+                End Try
+            End If
+        End Sub
+
+        Sub SetSliders()
+            windowSlider.Value = _dImage.DefaultWindow
+            levelSlider.Value = _dImage.DefaultLeveling
+        End Sub
+
+        Private Sub OpenItems(ByVal stm As Stream)
+            _stm = stm
+            If _stm Is Nothing Then
+                Return
+            End If
+            Try
+                ' Get the dataset
+                _dataSet = New DicomDataset(_stm)
+
+                ' Get the modality image
+                _dImage = _dataSet.GetModalityTransformedImage(0)
+
+                ' set the sliders to the values in the file
+                SetSliders()
+
+                ' get initial image, transformed to default window and leveling
+                Dim window As Integer = windowSlider.Value
+                If _dImage.ImageCameFromSignedSamples Then
+                    window += _dImage.ImageDataShiftedBy
+                End If
+                _image = _dImage.GetAtalaImage(window, Me.levelSlider.Value)
+                Me.imageViewer1.Image = _image
+            Catch err As Exception
+                CloseCurrentItems()
+                MessageBox.Show("Unable to open file: " + err.Message)
+            End Try
+        End Sub
+
+        Private Sub CloseCurrentItems()
+            ' Dispose and nullify everything
+            If _image IsNot Nothing Then
+                Me.imageViewer1.Image = Nothing
+                _image.Dispose()
+                _image = Nothing
+            End If
+            If _dImage IsNot Nothing Then
+                _dImage.Dispose()
+                _dImage = Nothing
+            End If
+            If _dataSet IsNot Nothing Then
+                _dataSet.Dispose()
+                _dataSet = Nothing
+            End If
+            If _stm IsNot Nothing Then
+                _stm.Close()
+                _stm = Nothing
+            End If
+        End Sub
+
+        Private Sub exitToolStripMenuItem_Click(ByVal sender As Object, ByVal e As EventArgs) Handles exitToolStripMenuItem.Click
+            Me.Close()
+        End Sub
+
+        Private Sub UpdateImage()
+            If _dImage IsNot Nothing AndAlso _image IsNot Nothing Then
+                ' get the window
+                Dim window As Integer = windowSlider.Value
+                If _dImage.ImageCameFromSignedSamples Then
+                    window += _dImage.ImageDataShiftedBy
+                End If
+
+                ' Get the image transformed by the window and level.
+                ' NOTE: this variant of GetAtalaImage does NOT allocate a new
+                ' AtalaImage, but instead writes into the one passed in, so
+                ' there is no need to call Dispose on the image
+                _dImage.GetAtalaImage(_image, window, Me.levelSlider.Value)
+
+                ' For images that may require a backing store of a more optimal
+                ' pixel format, Refresh() forces that backing store to be dropped,
+                ' if necessary, then it will call Invalidate()
+                imageViewer1.Refresh()
+            End If
+        End Sub
+
+        Private Sub windowSlider_ValueChanged(ByVal sender As Object, ByVal e As EventArgs) Handles windowSlider.ValueChanged
+            UpdateImage()
+        End Sub
+
+        Private Sub levelSlider_ValueChanged(ByVal sender As Object, ByVal e As EventArgs) Handles levelSlider.ValueChanged
+            UpdateImage()
+        End Sub
+
+        Private Sub HelpAboutToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles HelpAboutToolStripMenuItem.Click
+            Dim aboutBox As AtalaDemos.AboutBox.About = New AtalaDemos.AboutBox.About("About Atalasoft Dicom Leveling Demo", "DotImage Dicom Leveling")
+            aboutBox.Description = "This is a very basic demo that shows how to open and view a DICOM image using our DicomImage and DicomDataset classes to enable Window and Level control in a Winforms app.  " & vbCrLf & vbCrLf &
+                                    "If you're looking for display controls on DICOM images such as zoom, magnify and panning, or if you're looking for examples of viewing metadata on DICOM images, check out our Dicom Viewer Demo.  " & vbCrLf & vbCrLf &
+                                    "Requires evaluation or purchased licenses of DotImage Document Imaging or Photo Pro, as well as our Dicom Decoder addon."
+            aboutBox.ShowDialog()
+
+
+        End Sub
+    End Class
+End Namespace
